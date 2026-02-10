7289f3e55218e697bd25f67b6f86d3a4db510a0a
abdulroqibscybud <abdulroqeeboladipo@gmail.com>
2026-02-10 09:07:25 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,37 @@
+const taskEl = document.getElementById("task");
+const timeEl = document.getElementById("taskTime");
+const noteEl = document.getElementById("note");
+
+const createdLogs = document.getElementById("createdLogs");
+
+const logTaskBtn = document.getElementById("logTask");
+
+logTaskBtn.addEventListener("click", () => {
+  const taskValue = taskEl.value.trim();
+  const timeValue = timeEl.value.trim();
+  const noteValue = noteEl.value.trim();
+
+  const logData = {
+    taskValue,
+    timeValue,
+    noteValue,
+  };
+
+  const loggedTaskContainer = document.createElement("div");
+  loggedTaskContainer.classList.add("loggedTaskContainer");
+
+  const taskDetails = document.createElement("details");
+  const taskSummary = document.createElement("summary");
+
+  taskSummary.textContent = taskValue;
+
+  const taskTime = document.createElement("span");
+  taskTime.textContent = timeValue;
+
+  const taskNote = document.createElement("p");
+  taskNote.textContent = noteValue;
+
+  taskDetails.append(taskSummary, taskTime, taskNote);
+
+  createdLogs.prepend(taskDetails);
+});
